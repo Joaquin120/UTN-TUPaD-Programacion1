36ca26066da6ccd41fee17fa683d7b25a7a13a0e
Joaquin120 <97991063+Joaquin120@users.noreply.github.com>
2025-08-19 15:38:53 -0300

    Create tp1 secuenciales.py

---
+++ tp1 secuenciales.py
@@ -0,0 +1,65 @@
+#actividad 1
+print ("act 1")
+print ("Hola mundo")
+print ("      ")
+#actividad 2
+print ("act 2")
+nombre_a2=input("escribe tu nombre ")  #a1,a2,a3 y etc significa el numero de la actividad
+print(f"me llamo {nombre_a2}")
+print ("      ")
+#actividad 3
+print ("act 3")
+nombre_a3=input("escribe tu nombre ")
+apellido_a3=input("escribe tu apellido ")
+edad_a3=int(input("escribe tu edad "))
+lugar_de_residencia_a3=input("escribe tu lugar de residencia ")
+print (f"me llamo {nombre_a3} {apellido_a3} tengo {edad_a3} y vivo en {lugar_de_residencia_a3}")
+print ("       ")
+#actividad 4
+pi=3.14159
+radio_a4=int(input("escribe el radio del circulo "))
+area_a4=pi*(radio_a4**2)
+print (f"el area del circulo es {area_a4} cm")
+print ("   ")
+#actividad 5
+segundos_a5=int(input("escribe una cantidad de segundos "))
+horas=int(segundos_a5/60/60)
+print (f"{segundos_a5} segundos equivalen a {horas} horas")
+print("   ")
+#actividad 6
+num_a6=int(input("escribe el numero a multiplicar "))
+print (f"{num_a6}X1={num_a6*1}")
+print (f"{num_a6}X2={num_a6*2}")
+print (f"{num_a6}X3={num_a6*3}")
+print (f"{num_a6}X4={num_a6*4}")
+print (f"{num_a6}X5={num_a6*5}")
+print (f"{num_a6}X6={num_a6*6}")
+print (f"{num_a6}X7={num_a6*7}")
+print (f"{num_a6}X8={num_a6*8}")
+print (f"{num_a6}X9={num_a6*9}")
+print (f"{num_a6}X10={num_a6*10}")
+print("")
+#actividad 7
+a_a7=int(input("escribe un numero entero distinto de 0 "))
+b_a7=int(input("escribe el segundo numero entero distinto de 0 "))
+print (f"{a_a7}+{b_a7}={a_a7+b_a7}")
+print (f"{a_a7}/{b_a7}={a_a7/b_a7}")
+print (f"{a_a7}*{b_a7}={a_a7*b_a7}")
+print (f"{a_a7}-{b_a7}={a_a7-b_a7}")
+print ("")
+#actividad 8
+peso_a8=int(input("escribe tu peso en  kg "))
+altura_a8=float(input("escribe tu altura en metros "))
+calculo_a8=peso_a8/altura_a8**2
+print (f"tu indice de masa corporal es {calculo_a8}")
+print ("")
+#actividad 9
+celcius_a9=float(input("escribe cuantos grados celcius pasar a Fahrenheit "))
+fahremheit=(celcius_a9*1.8)+32
+print (f"{celcius_a9} celcius son {fahremheit} fahremheit")
+#actividad 10
+num1_a10=float(input("escribe el primer numero "))
+num2_a10=float(input("escribe el segundo numero "))
+num3_a10=float(input("escribe el tercer numero "))
+promedio_a10=(num1_a10+num2_a10+num3_a10)/3
+print (f"el promedio es de {promedio_a10}")
